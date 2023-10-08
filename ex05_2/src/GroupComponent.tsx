19080a4d1cc20e027309d2a86aace74a8b13694d
djdongjae <jk47wee@gmail.com>
2023-10-08 15:48:58 +0900

    02-05 렌더링과 todo앱 제작 2회독 진행중

---
+++ ex05_2/src/GroupComponent.tsx
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+import CounterA from "./CounterA";
+import CounterB from "./CounterB";
+
+function GroupComponent() {
+    console.log('GroupComponent 호출됨');
+    const [value, setValue] = useState<number>(0);
+    return (
+        <div className="box">
+            <h1>GroupComponent</h1>
+            <button onClick={() => setValue(value + 1)}>
+                {value}
+            </button>
+            <CounterA />
+            <CounterB />
+        </div>
+    )
+}
+
+export default GroupComponent;
