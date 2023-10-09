2f07a59ddfb033f03913be440f1c681a15ce495d
djdongjae <jk47wee@gmail.com>
2023-10-09 18:34:27 +0900

    02-05 렌더링과 todo앱 제작 2회독 진행중

--- ex05_2/src/App.tsx
+++ ex05_2/src/App.tsx
@@ -1,20 +1,27 @@
 import React, {useState} from "react";
-import "./App.css";
+import "./App.css"
+
+type Person = {
+  name : string,
+  age : number,
+  height : number
+}
 
 function App() {
-  const [arr, setArr] = useState<number[]>([0, 0]);
-  const increaseAt = (index: number) => {
-    arr[index] = arr[index] + 1;
-    setArr(arr);
+  const [person, setPerson] = useState<Person>({name: "홍길동", age : 16, height : 180});
+  const increase = (key : string) => {
+    if (key === "height") person.height++;
+    else if (key === "age") person.age++;
+    setPerson({...person});
   };
   return (
     <div className="box">
       <h1>App</h1>
-      <p>{arr.join(", ")}</p>
-      <button onClick={() => increaseAt(0)}>a[0]++</button>
-      <button onClick={() => increaseAt(1)}>a[1]++</button>
+      <p>{person.age}, {person.height}</p>
+      <button onClick={() => increase("age")}>age++</button>
+      <button onClick={() => increase("height")}>height++</button>
     </div>
-  );
+  )
 }
 
 export default App;
